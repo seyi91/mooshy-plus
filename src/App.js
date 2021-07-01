a894dd8850891f5ded53911a0929fab335b0b8dc
MADEYE <52187012+seyi91@users.noreply.github.com>
2021-07-01 13:35:04 -0500

    add NavMenu component to Header.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Header from './components/Header';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+       <Header />
         <Switch>
           <Route exact path="/">
             <Login />
